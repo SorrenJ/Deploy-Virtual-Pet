84a5fa629eef261f2f8952901209037b286ba964
SorrenJ <soalexjao@gmail.com>
2024-11-03 15:37:58 -0800

    change database pool to database_url

--- Virtual-Pet/server/db/db.js
+++ Virtual-Pet/server/db/db.js
@@ -4,7 +4,7 @@ require('dotenv').config(); // Optional, if using environment variables
 const pool = new Pool({
   user: process.env.DB_USER || 'labber',
   host: process.env.DB_HOST || 'localhost',
-  database: process.env.DB_NAME || 'beastly_bonds_development',
+  database: process.env.DATABASE_URL || 'beastly_bonds_development',
   password: process.env.DB_PASSWORD || 'labber',  // This should match DB_PASSWORD in .env
   port: process.env.DB_PORT || 5432, // No space around 5432
 });
