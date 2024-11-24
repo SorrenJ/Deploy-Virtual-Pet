54bfa906da8f92f3e2b30a8404de91408c391209
SorrenJ <soalexjao@gmail.com>
2024-11-23 18:35:38 -0800

    add mongoConnection and remove local connections

--- Virtual-Pet/server/db/mongoConnection.js
+++ Virtual-Pet/server/db/mongoConnection.js
@@ -1,13 +1,13 @@
 const { MongoClient } = require('mongodb');
 
-const uri = "mongodb+srv://soalexjao:Bionicle20!0@cluster0.yymbu.mongodb.net/";
+const uri = "mongodb+srv://soalexjao:Bionicle20!0@cluster0.yymbu.mongodb.net/beastly_bonds_development";
 
 let dbConnection;
 
 const connectToDatabase = async () => {
     if (!dbConnection) {
         const client = new MongoClient(uri);
-        dbConnection = client.db();
+        dbConnection = client.db("beastly_bonds_development");
         console.log("Connected to MongoDB");
     }
     return dbConnection;
